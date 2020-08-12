14009bd9c963a17de591c99aff2831dca44f47e0
DavidStreid <david.streid.nyc@gmail.com>
2020-08-12 12:58:15 -0400

    Switching to templates

---
+++ templates/detect_runs.sh
@@ -0,0 +1,21 @@
+#$/bin/bash
+
+sequencer_files=( ${SEQUENCER_DIR}/johnsawyers/*/RTAComplete.txt
+  ${SEQUENCER_DIR}/johnsawyers/*/RTAComplete.txt
+  ${SEQUENCER_DIR}/kim/*/RTAComplete.txt
+  ${SEQUENCER_DIR}/momo/*/RTAComplete.txt
+  ${SEQUENCER_DIR}/toms/*/RTAComplete.txt
+  ${SEQUENCER_DIR}/vic/*/RTAComplete.txt
+  ${SEQUENCER_DIR}/diana/*/CopyComplete.txt
+  ${SEQUENCER_DIR}/michelle/*/CopyComplete.txt
+  ${SEQUENCER_DIR}/jax/*/SequencingComplete.txt
+  ${SEQUENCER_DIR}/pitt/*/SequencingComplete.txt
+  ${SEQUENCER_DIR}/scott/*/RunCompletionStatus.xml
+  ${SEQUENCER_DIR}/ayyan/*/RTAComplete.txt
+)
+
+for file in ${sequencer_files[@]}; do
+  find $(ls $file) -mmin -${RUN_AGE} >> ${RUNS_TO_DEMUX_FILE}
+done
+
+cat ${RUNS_TO_DEMUX_FILE}
