bff124d898277024227f9f8b658dd4a0492c1b26
DavidStreid <david.streid.nyc@gmail.com>
2021-02-01 15:35:59 -0500

    Adding mask only if split_sample_sheet workflow created *_i7.csv

--- templates/demultiplex.sh
+++ templates/demultiplex.sh
@@ -10,6 +10,38 @@
 # Run: 
 #   RUN_TO_DEMUX_DIR=/igo/sequencers/michelle/200814_MICHELLE_0249_AHMNCJDRXX ./demultiplex.sh
 
+#########################################
+# Reads the RunInfo.xml of the RUN_TO_DEMUX_DIR to retrieve mask and assigns to MASK_OPT
+# Globals:
+#   RUN_TO_DEMUX_DIR - Absolute path of run to demux
+#########################################
+assign_MASK_OPT () {
+  #Deletes shortest match of $substring '/*Complet*' from back of $x
+  RUNPATH=$(echo ${RUN_TO_DEMUX_DIR%/*Complet*})		# ../PATH/TO/sequencers/johnsawyers/201113_JOHNSAWYERS_0252_000000000-G6H72
+  OLDIFS=$IFS
+  IFS='/'
+  array=($RUNPATH)					# ( PATH TO sequencers johnsawyers 201113_JOHNSAWYERS_0252_000000000-G6H72 )
+  MACHINE="${array[-2]}"				# johnsawyers
+  RUN_TO_DEMUX="${array[-1]}" 				# 201113_JOHNSAWYERS_0252_000000000-G6H72
+  IFS=${OLDIFS}
+
+  RUN_INFO_PATH=${RUNPATH}/RunInfo.xml
+  R1=$( cat $RUN_INFO_PATH | grep "Number=\"1\"" | awk '{posCycle=match($0,"s=");print substr($0,posCycle+3,5)}' | awk '{PosIndex=match($0,"\""); print substr($0,1,PosIndex-1)}')
+  R2=$( cat $RUN_INFO_PATH | grep "Number=\"2\"" | awk '{posCycle=match($0,"s=");print substr($0,posCycle+3,5)}' | awk '{PosIndex=match($0,"\""); print substr($0,1,PosIndex-1)}')
+  R3=$( cat $RUN_INFO_PATH | grep "Number=\"3\"" | awk '{posCycle=match($0,"s=");print substr($0,posCycle+3,5)}' | awk '{PosIndex=match($0,"\""); print substr($0,1,PosIndex-1)}')
+  R4=$( cat $RUN_INFO_PATH | grep "Number=\"4\"" | awk '{posCycle=match($0,"s=");print substr($0,posCycle+3,5)}' | awk '{PosIndex=match($0,"\""); print substr($0,1,PosIndex-1)}')
+ 
+  # Number of actual bases is one less than the number of cycles Reads section of RunInfo.xml
+  R1_bleed="$((R1 - 1))"
+  R4_bleed="$((R4 - 1))"
+
+  MASK="y${R1_bleed}n,i${R2},n${R3},y${R4_bleed}n"
+ 
+  echo "R1=${R1} R2=${R2} R3=${R3} R4=${R4} MASK: ${MASK}"
+  MASK_OPT="--use-bases-mask ${MASK}"
+}
+
+
 SAMPLESHEET=$(echo $SAMPLESHEET | tr -d " \t\n\r")	# Sometimes "\n" or "\t" characters can be appended 
 
 # DEFAULT JOB COMMANDS
@@ -49,7 +81,13 @@ if /bin/grep -q "10X_Genomics" $SAMPLESHEET; then
 else
   export LD_LIBRARY_PATH=/opt/common/CentOS_6/gcc/gcc-4.9.2/lib64:$LD_LIBRARY_PATH
   echo "DEMUX CMD (${RUN_BASENAME}): bcl2fastq"
-  JOB_CMD="/opt/common/CentOS_6/bcl2fastq/bcl2fastq2-v2.20.0.422/bin/bcl2fastq --minimum-trimmed-read-length 0 --mask-short-adapter-reads 0 --ignore-missing-bcl  --runfolder-dir  $RUN_TO_DEMUX_DIR --sample-sheet ${SAMPLESHEET} --output-dir ${DEMUXED_DIR} --ignore-missing-filter --ignore-missing-positions --ignore-missing-control --barcode-mismatches 1 --no-lane-splitting  --loading-threads 12 --processing-threads 24 >> ${BCL_LOG} 2>&1"
+  MASK_OPT=""
+  has_i7=$(echo ${SAMPLESHEET} | grep _i7.csv)
+  if [[ ! -z $has_i7 ]]; then
+    echo "Masking i5 index"
+    assign_MASK_OPT
+  fi
+  JOB_CMD="/opt/common/CentOS_6/bcl2fastq/bcl2fastq2-v2.20.0.422/bin/bcl2fastq ${MASK_OPT} --minimum-trimmed-read-length 0 --mask-short-adapter-reads 0 --ignore-missing-bcl  --runfolder-dir  $RUN_TO_DEMUX_DIR --sample-sheet ${SAMPLESHEET} --output-dir ${DEMUXED_DIR} --ignore-missing-filter --ignore-missing-positions --ignore-missing-control --barcode-mismatches 1 --no-lane-splitting  --loading-threads 12 --processing-threads 24 >> ${BCL_LOG} 2>&1"
 fi
 
 echo ${JOB_CMD}
@@ -88,3 +126,6 @@ else
 fi
 
 # TODO - Update: Add a notification for when a DEMUX fails. VERY IMPORTANT - Some sequencers (e.g. SCOTT) delete their old data w/ each new run, i.e. $30,000 run could be deleted just b/c the copy didn't work correctly
+
+
+
