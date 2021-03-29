ec348b9b4cf2955adf4e85b5d066c8193c098a9c
Igo User <igo@igo-ln01.igo.private>
2021-03-29 14:54:31 -0400

    Taking FASTQ_DIR & SEQUENCER_DIR from nextflow.config

--- crontab/detect_copied_sequencers.sh
+++ crontab/detect_copied_sequencers.sh
@@ -7,12 +7,20 @@ source ~/.bash_profile # load java, singularity, python, etc
 NUM_MINS_OLD=60
 EVENTS_API="http://dlviigoweb1:4500/api/nextflow/receive-nextflow-event"
 
-# DIR location should come from nextflow.config
-SEQ_DIR=/igo/sequencers/
-FASTQ_DIR=/igo/stats/NF_TESTING/FASTQ
-WORK_DIR=/igo/stats/NF_TESTING/working 
+LOCATION=$(dirname "$0")
+WHOAMI=$(basename "$0")
 
-RECENTLY_CREATED_RUN_DIRS=$(find ${SEQ_DIR} -mindepth 2 -maxdepth 2 -type d -mmin -${NUM_MINS_OLD})
+NEXTFLOW_CONFIG=${LOCATION}/../nextflow.config
+
+echo ""
+echo "[$(date "+%Y%m%d") $(date +"%T")] Running ${LOCATION}/${WHOAMI}"
+echo "Taking SEQUENCER_DIR & FASTQ_DIR from ${NEXTFLOW_CONFIG}"
+eval $(cat ${NEXTFLOW_CONFIG} | grep SEQUENCER_DIR)	# e.g. eval SEQUENCER_DIR="/igo/sequencers"
+eval $(cat ${NEXTFLOW_CONFIG} | grep FASTQ_DIR)		# e.g. eval FASTQ_DIR="/igo/stats/NF_TESTING/FASTQ"
+WORK_DIR=$(dirname ${FASTQ_DIR})/working
+echo "SEQUENCER_DIR=${SEQUENCER_DIR} FASTQ_DIR=${FASTQ_DIR} WORK_DIR=${WORK_DIR}"
+
+RECENTLY_CREATED_RUN_DIRS=$(find ${SEQUENCER_DIR} -mindepth 2 -maxdepth 2 -type d -mmin -${NUM_MINS_OLD})
 
 # We filter the previous find command by directories w/ FILES written within the past day.
 # For some reason, it looks like sequencer run folders are "touched" by res_igo_seq 
@@ -24,10 +32,6 @@ for DIR in ${RECENTLY_CREATED_RUN_DIRS}; do
   fi
 done
 
-LOCATION=$(dirname "$0")
-WHOAMI=$(basename "$0")
-
-echo "[$(date "+%Y%m%d") $(date +"%T")] Running ${LOCATION}/${WHOAMI}"
 echo "${#NEW_RUNS[@]} runs have been updaed in the past ${NUM_MINS_OLD} minute(s): ${NEW_RUNS[*]}"
 if [[ ${#NEW_RUNS[@]} -eq 0 ]]; then
   echo "Exiting."
@@ -38,17 +42,17 @@ echo "Checking whether each run has been copied over completely..."
 for RUN in ${NEW_RUNS[@]}; do
   printf "\tVERIFYING: $RUN\n"
   # Run the nextflow process script that determines if the run is good to be demultiplexed. On error, run is skipped
-  DEMUX_ALL=false FASTQ_DIR=${FASTQ_DIR} SEQUENCER_DIR=${SEQ_DIR} RUN=${RUN} "${LOCATION}/../templates/detect_runs.sh"
+  DEMUX_ALL=false FASTQ_DIR=${FASTQ_DIR} SEQUENCER_DIR=${SEQUENCER_DIR} RUN=${RUN} "${LOCATION}/../templates/detect_runs.sh" > /dev/null
   if [ $? -eq 0 ]; then
     RUNNAME=$(basename ${RUN})
     RUN_DIR=${WORK_DIR}/${RUNNAME}
     mkdir -p ${RUN_DIR}
     cd ${RUN_DIR}
-    echo "[DEMULTIPLEXING] RUN=${RUN} WORK_DIR=$(pwd)"
+    printf "\tDEMULTIPLEXING: RUN=${RUN} WORK_DIR=$(pwd)\n"
     nohup nextflow ${LOCATION}/../main.nf --run ${RUNNAME} -with-weblog "${EVENTS_API}" -bg >> "nf_${RUNNAME}.log"
     cd -
   else
-    echo "NOT DEMULTIPLEXING: ${RUN}"
+    printf "\tNOT DEMULTIPLEXING: ${RUN}\n"
   fi
   echo ""
 done
