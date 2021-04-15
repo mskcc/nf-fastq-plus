dd5f57930e8f054102410f1ec2036d8a220842b1
David Streid <david.streid.nyc@gmail.com>
2021-04-15 11:08:54 -0400

    Revert "Returning runs from 3-weeks"
    This reverts commit 445b4309f66784fa0d0237df24048db4551b367b.

--- dashboard/frontend/src/components/tracker-view.js
+++ dashboard/frontend/src/components/tracker-view.js
@@ -59,7 +59,7 @@ function TrackerView() {
     })();
   }, [sequencingRuns]);
   useEffect(() => {
-    getSequencingRuns(21).then((runs) => {
+    getSequencingRuns(14).then((runs) => {
       setSequencingRuns(runs);
     });
   }, []);
