e6cc7632d74137b5375ea0170af325cb50452742
David Streid <david.streid.nyc@gmail.com>
2021-04-19 16:01:54 -0400

    Adding input testing files

--- samplesheet_stats_main.nf
+++ samplesheet_stats_main.nf
@@ -2,8 +2,21 @@ nextflow.preview.dsl=2
 
 include { samplesheet_stats_wkflw } from './modules/samplesheet_stats';
 
+/**
+ * Processes input parameters if they are specified
+ */
+def process_param(pm, cValue) {
+  if( pm == null )
+    return cValue
+  return pm
+}
+
 DEMUXED_DIR=params.dir
 SAMPLESHEET=params.ss
+SEQUENCER_DIR=process_param(params.seq_dir, SEQUENCER_DIR)
+FASTQ_DIR=process_param(params.fq_dir, FASTQ_DIR)
+STATS_DIR=process_param(params.stats_dir, STATS_DIR)
+STATSDONEDIR=process_param(params.done_dir, STATSDONEDIR)
 
 println """\
           S A M P L E S H E E T    P I P E L I N E
---
+++ testPipeline/data/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+LOCATION=$(realpath $(dirname "$0"))
+cd ${LOCATION}
+
+# Write the directories
+SAMPLE_DIR=${LOCATION}/FASTQ/ROSALIND_0001_AGTCTGAGTC/Project_0001/Sample_U0a
+mkdir -p ${SAMPLE_DIR}
+cd ${SAMPLE_DIR}
+
+echo "Downloading FASTQs to ${SAMPLE_DIR}: U0a_CGATGT_L001_R1_001.fastq.gz, U0a_CGATGT_L001_R2_001.fastq.gz"
+wget -c ftp://ftp-trace.ncbi.nih.gov/ReferenceSamples/giab/data/NA12878/NIST_NA12878_HG001_HiSeq_300x/131219_D00360_005_BH814YADXX/Project_RM8398/Sample_U0a/U0a_CGATGT_L001_R2_001.fastq.gz
+wget -c ftp://ftp-trace.ncbi.nih.gov/ReferenceSamples/giab/data/NA12878/NIST_NA12878_HG001_HiSeq_300x/131219_D00360_005_BH814YADXX/Project_RM8398/Sample_U0a/U0a_CGATGT_L001_R1_001.fastq.gz
+
+echo "Subsampling"
+seqtk sample -s100 U0a_CGATGT_L001_R1_001.fastq.gz 500 > U0a_CGATGT_L001_R1_001_subsample.fastq.gz
+rm U0a_CGATGT_L001_R1_001.fastq.gz
+
+seqtk sample -s100 U0a_CGATGT_L001_R2_001.fastq.gz 500 > U0a_CGATGT_L001_R2_001_subsample.fastq.gz
+rm U0a_CGATGT_L001_R2_001.fastq.gz
+
+cd -
