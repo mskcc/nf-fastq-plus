ea2afe40a074893acf37e55729dd8c0a9088f4e4
DavidStreid <david.streid.nyc@gmail.com>
2021-04-22 17:16:51 -0400

    Adding relative path

--- bin/test/test_split_sample_sheet.sh
+++ bin/test/test_split_sample_sheet.sh
@@ -1,4 +1,6 @@
 #/bin/bash
 
-python ../create_multiple_sample_sheets.py --sample-sheet data/split_sampleSheet/original/SampleSheet_210422_ROSALIND_0001_FLOWCELLNAME.csv --processed-dir .
-python ../create_multiple_sample_sheets.py --sample-sheet data/split_sampleSheet/original/SampleSheet_201105_ROSALIND_0002_FLOWCELLNAME.csv --processed-dir .
+LOCATION=$(dirname "$0")
+
+python ${LOCATION}/../create_multiple_sample_sheets.py --sample-sheet ${LOCATION}/../data/split_sampleSheet/original/SampleSheet_210422_ROSALIND_0001_FLOWCELLNAME.csv --processed-dir .
+python ${LOCATION}/../create_multiple_sample_sheets.py --sample-sheet ${LOCATION}/../data/split_sampleSheet/original/SampleSheet_201105_ROSALIND_0002_FLOWCELLNAME.csv --processed-dir .
