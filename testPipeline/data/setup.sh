5d4a681f35762b5e11be77e9590a42aeecae2a89
David Streid <david.streid.nyc@gmail.com>
2021-04-27 12:18:30 -0400

    Refactoring FASTQ subsampling

--- testPipeline/data/setup.sh
+++ testPipeline/data/setup.sh
@@ -12,11 +12,12 @@ echo "Downloading FASTQs to ${SAMPLE_DIR}: U0a_CGATGT_L001_R1_001.fastq.gz, U0a_
 wget -c ftp://ftp-trace.ncbi.nih.gov/ReferenceSamples/giab/data/NA12878/NIST_NA12878_HG001_HiSeq_300x/131219_D00360_005_BH814YADXX/Project_RM8398/Sample_U0a/U0a_CGATGT_L001_R2_001.fastq.gz
 wget -c ftp://ftp-trace.ncbi.nih.gov/ReferenceSamples/giab/data/NA12878/NIST_NA12878_HG001_HiSeq_300x/131219_D00360_005_BH814YADXX/Project_RM8398/Sample_U0a/U0a_CGATGT_L001_R1_001.fastq.gz
 
-echo "Subsampling"
-seqtk sample -s100 U0a_CGATGT_L001_R1_001.fastq.gz 500 > U0a_CGATGT_L001_R1_001_subsample.fastq.gz
+SUBSAMPLE_AMOUNT=500
+echo "Subsampling (only ${SUBSAMPLE_AMOUNT} reads - something very small)"
+seqtk sample -s100 U0a_CGATGT_L001_R1_001.fastq.gz ${SUBSAMPLE_AMOUNT} > U0a_CGATGT_L001_R1_001_subsample.fastq.gz
 rm U0a_CGATGT_L001_R1_001.fastq.gz
 
-seqtk sample -s100 U0a_CGATGT_L001_R2_001.fastq.gz 500 > U0a_CGATGT_L001_R2_001_subsample.fastq.gz
+seqtk sample -s100 U0a_CGATGT_L001_R2_001.fastq.gz ${SUBSAMPLE_AMOUNT} > U0a_CGATGT_L001_R2_001_subsample.fastq.gz
 rm U0a_CGATGT_L001_R2_001.fastq.gz
 
 cd -
