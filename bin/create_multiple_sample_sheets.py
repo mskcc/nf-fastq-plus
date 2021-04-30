6057ee94a9b7a07eb2970dacb0c484761e42e8a0
David Streid <david.streid.nyc@gmail.com>
2021-04-30 16:57:24 -0400

    Adding creation of 6nt split

--- bin/create_multiple_sample_sheets.py
+++ bin/create_multiple_sample_sheets.py
@@ -6,6 +6,26 @@ import numpy as np
 import argparse
 import os
 
+""" Set of barcodes that should be masked to just the first 6 nucleotides
+"""
+BARCODE_6NT_SET = set()
+BARCODE_6NT_SET.update([ '{}'.format(i) for i in range(7001,7097) ])            # 7001-7096
+BARCODE_6NT_SET.update([ 'BC{}'.format(i) for i in range(1,9) ])                # BC1-8
+BARCODE_6NT_SET.update([ 'BK{}'.format(i) for i in range(25,44) ])              # BK25-43
+BARCODE_6NT_SET.update([ 'BK10{}'.format(i) for i in range(1,10) ])             # BK101-109
+BARCODE_6NT_SET.update([ 'BK{}'.format(i) for i in range(7098,7101) ])          # BK7098-7100
+BARCODE_6NT_SET.update([ 'Vakoc_{}'.format(i/10.0) for i in range(71,109) ])    # Vakoc_7.1-10.8
+BARCODE_6NT_SET.update([ 'TS{}'.format(i) for i in range(1,28) ])               # TS1-27
+BARCODE_6NT_SET.update([ 'Tsou_{}'.format(i) for i in range(1,13) ])            # Tsou_1-12
+BARCODE_6NT_SET.update([ 'RPI{}'.format(i) for i in range(1,49) ])              # RPI1-48
+BARCODE_6NT_SET.update([ 'NEBNext{}'.format(i) for i in range(1,28) ])          # NEBNext1-27
+BARCODE_6NT_SET.update([ 'NF{}'.format(i) for i in range(1,49) ])               # NF1-48
+BARCODE_6NT_SET.update([ 'Overholtzer_{}'.format(i) for i in range(1,5) ])      # Overholtzer_1-4
+BARCODE_6NT_SET.update([ 'KAPA_{}'.format(i) for i in range(1,13) ])            # KAPA_1-12
+BARCODE_6NT_SET.update([ 'Garippa_{}'.format(i) for i in range(2,46) ])         # Garippa_2-45
+BARCODE_6NT_SET.update([ 'IDT-TS{}'.format(i) for i in range(1,49) ])           # IDT-TS1-48
+BARCODE_6NT_SET.update([ 'DMP{}'.format(i) for i in range(1,49) ])              # DMP1-48
+
 def get_sample_sheet_name(sample_sheet):
 	""" Retrieves the samplesheet filename from the absolute path to the samplesheet
 	:param sample_sheet_file, str: absolute path to sample sheet
@@ -29,7 +49,7 @@ def tenx_genomics(sample_data, header):
 	# move regular sample sheet to the last element of the list	
 	tenx_genomics_data.index = range(len(tenx_genomics_data))
 	sample_data.index = range(len(sample_data))
-	data_sheets[5] = sample_data
+	data_sheets[6] = sample_data
 	if not tenx_genomics_data.empty:
 		data_sheets[0] = tenx_genomics_data
 
@@ -45,7 +65,7 @@ def dlp(sample_data, header):
 	# clean up index and  move regular sample sheet to the last element of the list
 	dlp_data.index = range(len(dlp_data))
 	sample_data.index = range(len(sample_data))
-	data_sheets[5] = sample_data
+	data_sheets[6] = sample_data
 	if not dlp_data.empty:
 		data_sheets[1] = dlp_data
 		
@@ -67,13 +87,46 @@ def wgs(sample_data, header):
 	wgs_data.index = range(len(wgs_data))
 	ped_peg_data.index = range(len(ped_peg_data))
 	sample_data.index = range(len(sample_data))
-	data_sheets[5] = sample_data
+	data_sheets[6] = sample_data
 	if not wgs_data.empty:
 		data_sheets[3] = wgs_data
 	if not ped_peg_data.empty:
 		data_sheets[4] = ped_peg_data
-			
-		
+
+def is_6nt_index(index_name):
+    """ Returns whether the input index_name is an index that should be masked to the first 6 nucleotides
+    :param index_name, str: value of index name     e.g. "7001"
+    :return: bool                                   e.g. True
+    """
+    return index_name in BARCODE_6NT_SET
+
+def has_6nt(sample_data, header):
+    """ Creates a 6_nt dataframe if 6nt indices are present
+            Side Effects:
+                - Modifies @sample_data in-place (Removes and re-orders indices)
+                - Creates a 6_nt dataframe and adds it to global @data_sheets
+
+    :param sample_data, df: original sample sheet
+    :param sample_data, header: original sample sheet headers
+
+    :return: bool, was a 6_nt dataframe created
+    """
+	# create empty data frame for padded requests
+	has_6nt_data = pd.DataFrame(columns = header)
+	for x in range(0, len(sample_data), 1):
+		i7_index = sample_data['I7_Index_ID'].loc[x]
+		if (is_6nt_index(i7_index)):
+			has_6nt_data.loc[x] = sample_data.loc[x]
+			sample_data.drop([x], inplace = True, axis = 0)
+
+	if not has_6nt_data.empty:
+		data_sheets[5] = has_6nt_data
+		# Reset the index to 0 (if this isn't done, the indices of sample_data will have missing positions that will cause errors)
+		sample_data.index = range(len(sample_data))
+		return True
+
+	return False
+
 def i7_only(sample_data, header):
 	# create empty data frame for padded requests
 	i7_data = pd.DataFrame(columns = header)
@@ -97,23 +150,22 @@ def i7_only(sample_data, header):
 	# move regular sample sheet to the last element of the list	
 	i7_data.index = range(len(i7_data))
 	sample_data.index = range(len(sample_data))
-	data_sheets[5] = sample_data
+	data_sheets[6] = sample_data
 	if not i7_data.empty:
 		data_sheets[2] = i7_data
 
-
 def create_csv(top_of_sheet, sample_sheet_name, processed_dir, created_sample_sheets = None):
 	# check to see if sample sheet has been manipulated in any way
-	if (data_sheets[0].equals(no_data)) and (data_sheets[1].equals(no_data)) and (data_sheets[2].equals(no_data)) and (data_sheets[3].equals(no_data)) and (data_sheets[4].equals(no_data)):
+	if (data_sheets[0].equals(no_data)) and (data_sheets[1].equals(no_data)) and (data_sheets[2].equals(no_data)) and (data_sheets[3].equals(no_data)) and (data_sheets[4].equals(no_data)) and (data_sheets[5].equals(no_data)):
 		print('NO CHANGES MADE TO THE ORIGINAL SAMPLE SHEET')
 	else:
 		print('WRITING NEW SAMPLE SHEETS: ' + processed_dir)
 	# list for sample sheet extensions
-	extensions = ['_10X.csv', '_DLP.csv', '_i7.csv', '_WGS.csv', '_PPG.csv', '.csv']
+	extensions = ['_10X.csv', '_DLP.csv', '_i7.csv', '_WGS.csv', '_PPG.csv', '_6nt.csv', '.csv']
 	
 	# go to new DividedSampleSheets directory
 	os.chdir(processed_dir)
-	
+
 	# create a csv sheet for all valid data sheets
 	for y in range(0, len(data_sheets), 1):
 		# break the loop in there were no changes in regular sample sheet or all of the samples were 10X, DLP or PADDED
@@ -121,9 +173,8 @@ def create_csv(top_of_sheet, sample_sheet_name, processed_dir, created_sample_sh
 			continue
 		else:
 			data_sheets[y].sort_values('Lane')
-			# print(data_element['Lane'])
 			data_element_list = data_sheets[y].T.reset_index().values.T.tolist()
-			# print(data_element_list)
+
 			# for BCL CONVERSION on DRAGEN, we must delete the "Adapter" tag in the SETTINGS section ( delete row 14 )
 			if y == 3:
 				# swap headings for SAMPLE_ID and SAMPLE_NAME ROWS
@@ -135,7 +186,7 @@ def create_csv(top_of_sheet, sample_sheet_name, processed_dir, created_sample_sh
 				data_element_sample_sheet = wgs_top_of_sheet + data_element_list
 			else:
 				data_element_sample_sheet = top_of_sheet + data_element_list
-			# ext = extensions[data_sheets.index(data_element)]
+
 			data_element_sample_sheet_name = sample_sheet_name + extensions[y]
 			print("Writing " + data_element_sample_sheet_name)
 			data_element_csv_file = open(data_element_sample_sheet_name, 'w')
@@ -167,27 +218,18 @@ def main():
 		output += ", OUTPUT FILE={}".format(created_sample_sheets)
 	print(output)
 	
-	# grab sample sheet - alternate way
-	# sample_sheet = args.sample_sheet.split('/')[5]
-	# sample_sheet_dir = args.sample_sheet[:-len(sample_sheet)]
-	# print('LOCATION OF SAMPLE SHEET = ' + sample_sheet_dir)
-	# print('SAMPLE SHEET = ' + sample_sheet)
-
-	
-	# print for sanity check - where are we?
-	print('LOCATION OF SAMPLE SHEET = ' + os.getcwd())
-	
 	# hold area for the sample sheet created
 	# index listing
-	# 0 = 10X, 1 = DLP, 2 = padded, 3 = HumanWholeGenome, 4 = PED-PEG, 5 = rest of sample sheet
+	# 0 = 10X, 1 = DLP, 2 = padded, 3 = HumanWholeGenome, 4 = PED-PEG, 5 = 6nt, 6 = rest of sample sheet
 	global data_sheets, no_data, dual_index
 	dual_index = True  
 	# empty data set for comparison
 	no_data = pd.DataFrame()
-	data_sheets = [no_data, no_data, no_data, no_data, no_data, no_data]
+	data_sheets = [no_data, no_data, no_data, no_data, no_data, no_data, no_data]
 		
 	# this will hold the top of the sample sheet
 	top_of_sheet = list()
+
 	# this will hold the sample data portion of the sheet
 	csv_sample_data = list()
 	barcode_types = list()
@@ -213,21 +255,25 @@ def main():
 
 	sample_sheet_name = get_sample_sheet_name(sample_sheet)
 
+	# 6nt barcodes
+	made_6nt_sample_sheet = has_6nt(sample_data, header)
+
 	# testing to see if we have dual barcodes, if not, we just quit.
 	# first check for 10X samples
 	if dual_index:
 		# check for 10X samples
-		tenx_genomics(sample_data, header)	   
-	
+		tenx_genomics(sample_data, header)
+
 		# call the DLP routine
-		dlp(data_sheets[5], header)
-		
+		dlp(data_sheets[6], header)
+
 		# routine for taking out HumanWholeGenome
-		wgs(data_sheets[5], header)
-		
+		wgs(data_sheets[6], header)
+
 		# check for padding
-		i7_only(data_sheets[5], header)
-	
+		i7_only(data_sheets[6], header)
+
+	if dual_index or made_6nt_sample_sheet:
 		# did we have to split sample sheets?
 		create_csv(top_of_sheet, sample_sheet_name, processed_dir, created_sample_sheets)
 	
--- bin/test/test_split_sample_sheet.sh
+++ bin/test/test_split_sample_sheet.sh
@@ -6,20 +6,21 @@ EXPECTED_DIR=${LOCATION}/data/split_sampleSheet/split
 SOURCE_DIR=${LOCATION}/data/split_sampleSheet/original
 
 function compare_files {
-  SOURCE=$1
-  TARGET_DIR=$2
-  EXPECTED=${@:3}
+  TYPE=$1
+  SOURCE=$2
+  TARGET_DIR=$3
+  EXPECTED=${@:4}
 
   for f in ${EXPECTED}; do
     fname=${LOCATION}/$(basename $f)
     # Test that file was written
     if [ ! -f ${fname} ]; then
-      echo "${fname}___Not_written"
+      echo "${TYPE}___${fname}___Not_written"
       exit 1
     fi
     diffs=$(diff $fname ${f})
     if [[ ! -z $diffs ]]; then
-      echo "${fname}___Different"
+      echo "${TYPE}___${fname}___Different"
       exit 1
     fi
   done
@@ -38,23 +39,26 @@ function compare_files {
 }
 
 ERRORS=""
-echo "Testing DLP WGS split"
+TYPE="DLP_WGS"
+echo "Testing ${TYPE} split"
 SOURCE_FILE=${SOURCE_DIR}/SampleSheet_210422_ROSALIND_0001_FLOWCELLNAME.csv
 EXPECTED_FILES=( ${EXPECTED_DIR}/SampleSheet_210422_ROSALIND_0001_FLOWCELLNAME.csv ${EXPECTED_DIR}/SampleSheet_210422_ROSALIND_0001_FLOWCELLNAME_DLP.csv ${EXPECTED_DIR}/SampleSheet_210422_ROSALIND_0001_FLOWCELLNAME_WGS.csv )
 python3 ${LOCATION}/../create_multiple_sample_sheets.py --sample-sheet ${SOURCE_FILE} --processed-dir ${LOCATION} > /dev/null
-ERRORS="${ERRORS}$(compare_files ${SOURCE_FILE} ${LOCATION} ${EXPECTED_FILES[@]})\n"
+ERRORS="${ERRORS}$(compare_files ${TYPE} ${SOURCE_FILE} ${LOCATION} ${EXPECTED_FILES[@]})\n"
 
-echo "Testing i7 split"
+TYPE="i7"
+echo "Testing ${TYPE} split"
 SOURCE_FILE=${SOURCE_DIR}/SampleSheet_201105_ROSALIND_0002_FLOWCELLNAME.csv
 EXPECTED_FILES=( ${EXPECTED_DIR}/SampleSheet_201105_ROSALIND_0002_FLOWCELLNAME.csv ${EXPECTED_DIR}/SampleSheet_201105_ROSALIND_0002_FLOWCELLNAME_i7.csv )
 python3 ${LOCATION}/../create_multiple_sample_sheets.py --sample-sheet ${SOURCE_FILE} --processed-dir ${LOCATION[@]} > /dev/null
-ERRORS="${ERRORS}$(compare_files ${SOURCE_FILE} ${LOCATION} ${EXPECTED_FILES[@]})\n"
+ERRORS="${ERRORS}$(compare_files ${TYPE} ${SOURCE_FILE} ${LOCATION} ${EXPECTED_FILES[@]})\n"
 
-echo "Testing 10X split"
+TYPE="10X"
+echo "Testing ${TYPE} split"
 SOURCE_FILE=${SOURCE_DIR}/SampleSheet_210421_ROSALIND_0003_FLOWCELLNAME.csv
 EXPECTED_FILES=( ${EXPECTED_DIR}/SampleSheet_210421_ROSALIND_0003_FLOWCELLNAME_10X.csv )
 python3 ${LOCATION}/../create_multiple_sample_sheets.py --sample-sheet ${SOURCE_FILE} --processed-dir ${LOCATION} > /dev/null
-ERRORS="${ERRORS}$(compare_files ${SOURCE_FILE} ${LOCATION} ${EXPECTED_FILES[@]})\n"
+ERRORS="${ERRORS}$(compare_files ${TYPE} ${SOURCE_FILE} ${LOCATION} ${EXPECTED_FILES[@]})\n"
 
 OUTPUT=$(printf "$ERRORS" | grep -v "success" | sed "s/^/   /")
 rm -rf ${LOCATION}/SampleSheet_*ROSALIND*_FLOWCELLNAME*.csv
