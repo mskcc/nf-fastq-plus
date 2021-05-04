d4248ce72d7461124b0fe0b8728c3504a9cfa4c2
David Streid <david.streid.nyc@gmail.com>
2021-05-04 14:46:23 -0400

    Moving all demux output to else condition (not DEMUX_ALL=true)

--- templates/demultiplex.sh
+++ templates/demultiplex.sh
@@ -57,6 +57,8 @@ assign_MASK_OPT () {
   echo "R1=${R1} R2=${R2} R3=${R3} R4=${R4} MASK: ${MASK}"
   MASK_OPT="--use-bases-mask ${MASK}"
 }
+BCL_LOG="bcl2fastq.log"
+
 SAMPLESHEET=$(echo $SAMPLESHEET | tr -d " \t\n\r")	# Sometimes "\n" or "\t" characters can be appended 
 
 # DEFAULT JOB COMMANDS
@@ -72,110 +74,103 @@ basename ${SAMPLESHEET}
 RUN_BASENAME=$(basename ${SAMPLESHEET} | grep -oP "(?<=[0-9]_)[A-Za-z_0-9-]+") # Capture after "[ANY NUM]_" (- ".csv")
 echo "RUN_BASENAME: ${RUN_BASENAME}"
 DEMUXED_DIR="${FASTQ_DIR}/${RUN_BASENAME}"
-
 mkdir -p $DEMUXED_DIR
-chmod -R 775 $DEMUXED_DIR
-cp $SAMPLESHEET $DEMUXED_DIR
-echo "Writing FASTQ files to $DEMUXED_DIR"
-echo "SAMPLESHEET: ${SAMPLESHEET}"
-
-JOB_CMD="echo NO_JOB_SPECIFIED"
-
-BCL_LOG="bcl2fastq.log"
-
-if grep -q "10X_Genomics" $SAMPLESHEET; then
-  export LD_LIBRARY_PATH=/opt/common/CentOS_6/gcc/gcc-4.9.2/lib64:$LD_LIBRARY_PATH
-  export PATH=$(dirname ${BCL2FASTQ}):$PATH
-  if grep -q "10X_Genomics_ATAC" $SAMPLESHEET; then
-    echo "DEMUX CMD (${RUN_BASENAME}): cellranger-atac mkfastq"
-    JOB_CMD="${CELL_RANGER_ATAC} mkfastq --input-dir ${RUN_TO_DEMUX_DIR} --sample-sheet ${SAMPLESHEET} --output-dir ${DEMUXED_DIR} --nopreflight --jobmode=lsf --mempercore=32 --disable-ui --maxjobs=200 --barcode-mismatches 1 >> ${BCL_LOG}"
-  else
-    echo "DEMUX CMD (${RUN_BASENAME}): cellranger mkfastq"
-    JOB_CMD="/igo/work/bin/cellranger-4.0.0/cellranger mkfastq --input-dir $RUN_TO_DEMUX_DIR/ --sample-sheet ${SAMPLESHEET} --output-dir ${DEMUXED_DIR} --nopreflight --jobmode=local --localmem=216 --localcores=36  --barcode-mismatches 1 >> ${BCL_LOG}"
-  fi
-else
-  export LD_LIBRARY_PATH=/opt/common/CentOS_6/gcc/gcc-4.9.2/lib64:$LD_LIBRARY_PATH
-  echo "DEMUX CMD (${RUN_BASENAME}): bcl2fastq"
-
-  # Add options depending on whether bin/create_multiple_sample_sheets.py created special sample sheets
-  MASK_OPT=""         # Option for use-bases-mask, default to no mask (will take from RunInfo.xml)
-  LANE_SPLIT_OPT=""   # Option for lane-splitting, default to lane-splitting
-  has_i7=$(echo ${SAMPLESHEET} | grep _i7.csv)
-  has_6nt=$(echo ${SAMPLESHEET} | grep _6nt.csv)
-  no_lane_split=$(echo ${SAMPLESHEET} | grep -E '_PPG.csv|_DLP.csv')
-  if [[ ! -z $has_i7 ]]; then
-    echo "Detected an _i7.csv SampleSheet. Will add mask to remove i5 index"
-    assign_MASK_OPT
-  elif [[ ! -z $has_6nt ]]; then
-    echo "Detected a _6nt.csv SampleSheet. Will add mask of six-nucleotide i7 index and to remove i5 index"
-    assign_MASK_OPT 6
-  elif [[ ! -z $no_lane_split ]]; then
-    echo "Detected a _PPG.csv or _DLP.csv SampleSheet. Using --no-lane-splitting option"
-    LANE_SPLIT_OPT="--no-lane-splitting"
-  fi
-
-  # detect_barcode_collision.py should be in bin dir of root of project
-  BARCODE_MISMATCH=1
-  CMD="detect_barcode_collision.py -s ${SAMPLESHEET} -m ${BARCODE_MISMATCH}"
-  echo $CMD
-  eval $CMD
-  if [ $? -ne 0 ]; then
-    BARCODE_MISMATCH=0
-  fi
-
-  echo "Running bcl2fastq w/ mismatches=${BARCODE_MISMATCH}"
-  JOB_CMD="${BCL2FASTQ} ${MASK_OPT} ${LANE_SPLIT_OPT} --minimum-trimmed-read-length 0 --mask-short-adapter-reads 0 --ignore-missing-bcl  --runfolder-dir  $RUN_TO_DEMUX_DIR --sample-sheet ${SAMPLESHEET} --output-dir ${DEMUXED_DIR} --ignore-missing-filter --ignore-missing-positions --ignore-missing-control --barcode-mismatches ${BARCODE_MISMATCH} --loading-threads 12 --processing-threads 24 >> ${BCL_LOG} 2>&1"
-fi
-
-echo ${JOB_CMD} >> ${CMD_FILE}
-
 DEMUXED_FASTQS=$(find ${DEMUXED_DIR} -type f -name "*.fastq.gz")
 
-# Disable error - we want the output of ${BCL_LOG} logged somewhere. We want to alert on failed demux below
-set +e
 if [[ "${DEMUX_ALL}" == "true" && ! -z $DEMUXED_FASTQS  ]]; then
   LOG="Skipping demux (DEMUX_ALL=${DEMUX_ALL}) of already demuxed directory: ${DEMUXED_DIR}"
   echo $LOG >> ${BCL_LOG}
 else
+  chmod -R 775 $DEMUXED_DIR
+  cp $SAMPLESHEET $DEMUXED_DIR
+  echo "Writing FASTQ files to $DEMUXED_DIR"
+  echo "SAMPLESHEET: ${SAMPLESHEET}"
+  JOB_CMD="echo NO_JOB_SPECIFIED"
+  if grep -q "10X_Genomics" $SAMPLESHEET; then
+    export LD_LIBRARY_PATH=/opt/common/CentOS_6/gcc/gcc-4.9.2/lib64:$LD_LIBRARY_PATH
+    export PATH=$(dirname ${BCL2FASTQ}):$PATH
+    if grep -q "10X_Genomics_ATAC" $SAMPLESHEET; then
+      echo "DEMUX CMD (${RUN_BASENAME}): cellranger-atac mkfastq"
+      JOB_CMD="${CELL_RANGER_ATAC} mkfastq --input-dir ${RUN_TO_DEMUX_DIR} --sample-sheet ${SAMPLESHEET} --output-dir ${DEMUXED_DIR} --nopreflight --jobmode=lsf --mempercore=32 --disable-ui --maxjobs=200 --barcode-mismatches 1 >> ${BCL_LOG}"
+    else
+      echo "DEMUX CMD (${RUN_BASENAME}): cellranger mkfastq"
+      JOB_CMD="/igo/work/bin/cellranger-4.0.0/cellranger mkfastq --input-dir $RUN_TO_DEMUX_DIR/ --sample-sheet ${SAMPLESHEET} --output-dir ${DEMUXED_DIR} --nopreflight --jobmode=local --localmem=216 --localcores=36  --barcode-mismatches 1 >> ${BCL_LOG}"
+    fi
+  else
+    export LD_LIBRARY_PATH=/opt/common/CentOS_6/gcc/gcc-4.9.2/lib64:$LD_LIBRARY_PATH
+    echo "DEMUX CMD (${RUN_BASENAME}): bcl2fastq"
+
+    # Add options depending on whether bin/create_multiple_sample_sheets.py created special sample sheets
+    MASK_OPT=""         # Option for use-bases-mask, default to no mask (will take from RunInfo.xml)
+    LANE_SPLIT_OPT=""   # Option for lane-splitting, default to lane-splitting
+    has_i7=$(echo ${SAMPLESHEET} | grep _i7.csv)
+    has_6nt=$(echo ${SAMPLESHEET} | grep _6nt.csv)
+    no_lane_split=$(echo ${SAMPLESHEET} | grep -E '_PPG.csv|_DLP.csv')
+    if [[ ! -z $has_i7 ]]; then
+      echo "Detected an _i7.csv SampleSheet. Will add mask to remove i5 index"
+      assign_MASK_OPT
+    elif [[ ! -z $has_6nt ]]; then
+      echo "Detected a _6nt.csv SampleSheet. Will add mask of six-nucleotide i7 index and to remove i5 index"
+      assign_MASK_OPT 6
+    elif [[ ! -z $no_lane_split ]]; then
+      echo "Detected a _PPG.csv or _DLP.csv SampleSheet. Using --no-lane-splitting option"
+      LANE_SPLIT_OPT="--no-lane-splitting"
+    fi
+
+    # detect_barcode_collision.py should be in bin dir of root of project
+    BARCODE_MISMATCH=1
+    CMD="detect_barcode_collision.py -s ${SAMPLESHEET} -m ${BARCODE_MISMATCH}"
+    echo $CMD
+    eval $CMD
+    if [ $? -ne 0 ]; then
+      BARCODE_MISMATCH=0
+    fi
+
+    echo "Running bcl2fastq w/ mismatches=${BARCODE_MISMATCH}"
+    JOB_CMD="${BCL2FASTQ} ${MASK_OPT} ${LANE_SPLIT_OPT} --minimum-trimmed-read-length 0 --mask-short-adapter-reads 0 --ignore-missing-bcl  --runfolder-dir  $RUN_TO_DEMUX_DIR --sample-sheet ${SAMPLESHEET} --output-dir ${DEMUXED_DIR} --ignore-missing-filter --ignore-missing-positions --ignore-missing-control --barcode-mismatches ${BARCODE_MISMATCH} --loading-threads 12 --processing-threads 24 >> ${BCL_LOG} 2>&1"
+  fi
+  echo ${JOB_CMD} >> ${CMD_FILE}
+
   echo "Running demux"
+  # Disable error - we want the output of ${BCL_LOG} logged somewhere. We want to alert on failed demux below
+  set +e
   eval ${JOB_CMD}
-fi
-UNDETERMINED_SIZE=$(du -sh  ${DEMUXED_DIR}/Undet*);
-PROJECT_SIZE=$(du -sh ${DEMUXED_DIR}/Proj*/*);
-set -e
+  UNDETERMINED_SIZE=$(du -sh  ${DEMUXED_DIR}/Undet*);
+  PROJECT_SIZE=$(du -sh ${DEMUXED_DIR}/Proj*/*);
+  set -e
 
-cat ${BCL_LOG} >> ${DEMUX_LOG_FILE}
-cat ${BCL_LOG}
+  cat ${BCL_LOG} >> ${DEMUX_LOG_FILE}
+  cat ${BCL_LOG}
 
-# TODO - Add a filtering process to determine which demux files are valid since it's possible for a job to have failed
-# NEXTFLOW ENVIRONMENT VARIABLES - These environment variables are passed to the next nextflow process
-echo "Demultiplexed DEMUXED_DIR: ${DEMUXED_DIR}, SAMPLESHEET: ${SAMPLESHEET}"
-FILE_OUTPUT_SIZE=$(printf "%s\n\n%s\n" "${UNDETERMINED_SIZE}" "$Proj_Size")
-REPORT="To view reports visit: ${DEMUXED_DIR}/Reports/html/index.html"
-FULL=$(printf "%s\n\n%s\n" "$FILE_OUTPUT_SIZE" "$REPORT")
+  # TODO - Add a filtering process to determine which demux files are valid since it's possible for a job to have failed
+  # NEXTFLOW ENVIRONMENT VARIABLES - These environment variables are passed to the next nextflow process
+  echo "Demultiplexed DEMUXED_DIR: ${DEMUXED_DIR}, SAMPLESHEET: ${SAMPLESHEET}"
+  FILE_OUTPUT_SIZE=$(printf "%s\n\n%s\n" "${UNDETERMINED_SIZE}" "$Proj_Size")
+  REPORT="To view reports visit: ${DEMUXED_DIR}/Reports/html/index.html"
+  FULL=$(printf "%s\n\n%s\n" "$FILE_OUTPUT_SIZE" "$REPORT")
 
-echo "DEMUX_UPDATE: ${FULL}"
+  echo "DEMUX_UPDATE: ${FULL}"
+  if [ -n "$FILE_OUTPUT_SIZE" ]; then
+    echo "MAIL: Starting stats for run ${RUN_BASENAME} ${DATA_TEAM_EMAIL}"
+    echo $FULL | mail -s "[SUCCESSFUL DEMUX] Starting stats for run ${RUN_BASENAME}" ${DATA_TEAM_EMAIL}
+  else
+    # Important Notification - Some sequencers (e.g. SCOTT) delete their old data w/ each new run, i.e. $30,000 run could be deleted just b/c the copy didn't work correctly
+    echo "MAIL: Failed Demux Run ${RUN_TO_DEMUX} ${DATA_TEAM_EMAIL}"
+    echo $FULL | mail -s "[FAILED DEMUX] ${RUN_TO_DEMUX}" ${DATA_TEAM_EMAIL}
+    exit 1
+  fi
 
-if [ -n "$FILE_OUTPUT_SIZE" ]; then
-  echo "MAIL: Starting stats for run ${RUN_BASENAME} ${DATA_TEAM_EMAIL}"
-  echo $FULL | mail -s "[SUCCESSFUL DEMUX] Starting stats for run ${RUN_BASENAME}" ${DATA_TEAM_EMAIL}
-else
-  # Important Notification - Some sequencers (e.g. SCOTT) delete their old data w/ each new run, i.e. $30,000 run could be deleted just b/c the copy didn't work correctly
-  echo "MAIL: Failed Demux Run ${RUN_TO_DEMUX} ${DATA_TEAM_EMAIL}"
-  echo $FULL | mail -s "[FAILED DEMUX] ${RUN_TO_DEMUX}" ${DATA_TEAM_EMAIL}
-  exit 1
-fi
-
-# Add DLP metadata.yaml script
-is_dlp=$(echo ${SAMPLESHEET} | grep -E '_DLP.csv')
-if [[ ! -z $is_dlp && ! -z ${SHARED_SINGLE_CELL_DIR} && -d ${SHARED_SINGLE_CELL_DIR} ]]; then
-  echo "Detected an _DLP.csv SampleSheet. Creating metadata.yaml"
-  cd ${SHARED_SINGLE_CELL_DIR}
-  project_dirs=$(find ${DEMUXED_DIR} -maxdepth 1 -type d -name "Project_*")
-  for project_path in $project_dirs; do
-    prj=$(basename ${project_path})
-    CMD="make create-metadata-yaml ss=${SAMPLESHEET} prj=${prj} project_path=${project_path}"
-    echo ${CMD}
-    eval ${CMD}
-  done
+  # Add DLP metadata.yaml script
+  is_dlp=$(echo ${SAMPLESHEET} | grep -E '_DLP.csv')
+  if [[ ! -z $is_dlp && ! -z ${SHARED_SINGLE_CELL_DIR} && -d ${SHARED_SINGLE_CELL_DIR} ]]; then
+    echo "Detected an _DLP.csv SampleSheet. Creating metadata.yaml"
+    cd ${SHARED_SINGLE_CELL_DIR}
+    project_dirs=$(find ${DEMUXED_DIR} -maxdepth 1 -type d -name "Project_*")
+    for project_path in $project_dirs; do
+      prj=$(basename ${project_path})
+      CMD="make create-metadata-yaml ss=${SAMPLESHEET} prj=${prj} project_path=${project_path}"
+      echo ${CMD}
+      eval ${CMD}
+    done
+  fi
 fi
