d964a15479991932abd6588fd928beadd41c3073
David Streid <david.streid.nyc@gmail.com>
2021-05-25 12:51:36 -0400

    Fixing cellranger tests

--- testPipeline/templates/cellranger/cellranger_test.sh
+++ testPipeline/templates/cellranger/cellranger_test.sh
@@ -5,6 +5,10 @@ OUTPUT_FILE=output.txt
 LOCATION=$(dirname "$0")
 cd ${LOCATION}
 
+FASTQ_DIR=$(pwd)
+FASTQ_RUN_DIR=${FASTQ_DIR}/RUN
+FASTQ_SAMPLE_DIR=${FASTQ_RUN_DIR}/Project_10001/Sample_ESC_IGO_00001_1
+
 #########################################
 # Runs the cellranger command w/ test inputs
 # Globals:
@@ -27,9 +31,13 @@ run_cellranger() {
     STATS_DIR=$(pwd) ../../../templates/cellranger.sh > ${OUTPUT_FILE}
 }
 
+# Need to setup a mock FASTQ directory
+mkdir -p ${FASTQ_SAMPLE_DIR}
+touch ${FASTQ_SAMPLE_DIR}/Sample_ESC_IGO_00001_1_R1.fastq.gz
+touch ${FASTQ_SAMPLE_DIR}/Sample_ESC_IGO_00001_1_R2.fastq.gz
 
 run_cellranger GeneExpression
-EXPECTED_OUT="count --id=ESC_IGO_00001_1 --transcriptome=/igo/work/nabors/genomes/10X_Genomics/GEX/refdata-gex-GRCh38-2020-A --fastqs= --nopreflight --jobmode=lsf --mempercore=64 --disable-ui --maxjobs=200"
+EXPECTED_OUT="count --id=ESC_IGO_00001_1 --transcriptome=/igo/work/nabors/genomes/10X_Genomics/GEX/refdata-gex-GRCh38-2020-A --fastqs=/Users/streidd/work/nf-fastq-plus/testPipeline/templates/cellranger/RUN/Project_10001/Sample_ESC_IGO_00001_1 --nopreflight --jobmode=lsf --mempercore=64 --disable-ui --maxjobs=200"
 ACTUAL_OUT=$(cat ${OUTPUT_FILE})
 if [[ ! -z $(echo ${ACTUAL_OUT} | grep "${EXPECTED_OUT}") ]]; then
   echo "Passed ${TEST}: ${EXPECTED_OUT}"
@@ -41,7 +49,7 @@ else
 fi
 
 run_cellranger vdj
-EXPECTED_OUT="vdj --id=ESC_IGO_00001_1 --reference=/igo/work/nabors/genomes/10X_Genomics/VDJ/refdata-cellranger-vdj-GRCh38-alts-ensembl-2.0.0 --fastqs= --sample=ESC_IGO_00001_1 --nopreflight --jobmode=lsf --mempercore=64 --disable-ui --maxjobs=200"
+EXPECTED_OUT="vdj --id=ESC_IGO_00001_1 --reference=/igo/work/nabors/genomes/10X_Genomics/VDJ/refdata-cellranger-vdj-GRCh38-alts-ensembl-2.0.0 --fastqs=/Users/streidd/work/nf-fastq-plus/testPipeline/templates/cellranger/RUN/Project_10001/Sample_ESC_IGO_00001_1 --sample=ESC_IGO_00001_1 --nopreflight --jobmode=lsf --mempercore=64 --disable-ui --maxjobs=200"
 ACTUAL_OUT=$(cat ${OUTPUT_FILE})
 if [[ ! -z $(echo ${ACTUAL_OUT} | grep "${EXPECTED_OUT}") ]]; then
   echo "Passed ${TEST}: ${EXPECTED_OUT}"
@@ -53,7 +61,7 @@ else
 fi
 
 run_cellranger atac
-EXPECTED_OUT="count --id=ESC_IGO_00001_1 --fastqs= --reference=/igo/work/nabors/genomes/10X_Genomics/ATAC/refdata-cellranger-atac-GRCh38-1.0.1 --nopreflight --jobmode=lsf --mempercore=64 --disable-ui --maxjobs=200"
+EXPECTED_OUT="count --id=ESC_IGO_00001_1 --fastqs=/Users/streidd/work/nf-fastq-plus/testPipeline/templates/cellranger/RUN/Project_10001/Sample_ESC_IGO_00001_1 --reference=/igo/work/nabors/genomes/10X_Genomics/ATAC/refdata-cellranger-atac-GRCh38-1.0.1 --nopreflight --jobmode=lsf --mempercore=64 --disable-ui --maxjobs=200"
 ACTUAL_OUT=$(cat ${OUTPUT_FILE})
 if [[ ! -z $(echo ${ACTUAL_OUT} | grep "${EXPECTED_OUT}") ]]; then
   echo "Passed ${TEST}: ${EXPECTED_OUT}"
@@ -63,3 +71,6 @@ else
   echo "Failed ${TEST}: ${ACTUAL_OUT}"
   exit 1
 fi
+
+echo "All Tests Pass - Removing ${FASTQ_RUN_DIR}"
+rm -rf ${FASTQ_RUN_DIR}
