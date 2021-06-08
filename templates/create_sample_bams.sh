e33ebd1d147598ebcd1393c66a30edef0ed1cf25
David Streid <david.streid.nyc@gmail.com>
2021-06-08 07:35:26 -0400

    Fixing find

--- templates/create_sample_bams.sh
+++ templates/create_sample_bams.sh
@@ -15,6 +15,6 @@ source ${TEMPLATE_DIR}/utils/utils.sh
 SAMPLE_TAG=$(parse_param ${RUN_PARAMS_FILE} SAMPLE_TAG)
 
 # Retrieve all the samplesheets that contain this sample
-SAMPLE_SHEETS=$(find ${LAB_SAMPLE_SHEET_DIR} -type f -name "SampleSheet*" -exec grep -l "\d,[A-Za-z_]*${SAMPLE_TAG},.*${PROJECT_TAG}")
+SAMPLE_SHEETS=$(find ${LAB_SAMPLE_SHEET_DIR} -type f -name "SampleSheet*" -exec grep -l "\d,[A-Za-z_]*${SAMPLE_TAG},.*${PROJECT_TAG}" \;)
 
 # TODO - verify this
