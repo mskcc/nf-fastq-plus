917e928cf1038e03378a46760449ac10b8bf5a76
David Streid <david.streid.nyc@gmail.com>
2021-07-01 11:39:57 -0400

    fixing tests

--- testPipeline/templates/retrieve_all_sample_runs/retrieve_all_sample_runs_test.sh
+++ testPipeline/templates/retrieve_all_sample_runs/retrieve_all_sample_runs_test.sh
@@ -19,7 +19,14 @@ clean() {
   rm -rf ${FASTQ_DIR} && rm -rf ${ARCHIVED_DIR} && rm -rf ${PROCESSED_SAMPLE_SHEET_DIR} && rm *.txt && rm *.csv
 }
 
-simple_test_setup() {
+prj_only_in_current_run_setup() {
+  touch ${RUN_DIR}/SampleSheet_210628_ROSALIND_0001_FLOWCELL.csv
+  OLD_RUN_DIR=${FASTQ_DIR}/ROSALIND_0002_FLOWCELL/
+  mkdir -p ${OLD_RUN_DIR}
+  touch ${OLD_RUN_DIR}/SampleSheet_210621_ROSALIND_0002_FLOWCELL.csv
+}
+
+prjs_in_old_run_setup() {
   touch ${RUN_DIR}/SampleSheet_210628_ROSALIND_0001_FLOWCELL.csv
   OLD_RUN_DIR=${FASTQ_DIR}/ROSALIND_0002_FLOWCELL/
   OLD_PRJ_DIR=${OLD_RUN_DIR}/${PRJ}
@@ -28,14 +35,15 @@ simple_test_setup() {
 }
 
 cd ${LOCATION}
+
 make_dirs
-simple_test_setup
+prj_only_in_current_run_setup
 DEMUXED_DIR=${RUN_DIR} FASTQ_DIR=${FASTQ_DIR} ARCHIVED_DIR=${ARCHIVED_DIR} \
   PROCESSED_SAMPLE_SHEET_DIR=${PROCESSED_SAMPLE_SHEET_DIR} RUNNAME=${RUN} ../../../templates/retrieve_all_sample_runs.sh
 
 OUTPUT_SS=run_samplesheet.txt
 
-EXPECTED_ENTRIES=2
+EXPECTED_ENTRIES=0
 if [[ ${EXPECTED_ENTRIES} -eq $(cat ${OUTPUT_SS} | wc -l) ]]; then
   echo "Expected number of Entries"
 else
@@ -43,7 +51,7 @@ else
   exit 1
 fi
 
-EXPECTED_RUNS="ROSALIND_0001_FLOWCELL ROSALIND_0002_FLOWCELL"
+EXPECTED_RUNS=""
 for expected_run in ${EXPECTED_RUNS}; do
   if [[ -z $(cat ${OUTPUT_SS} | grep ${expected_run} ) ]]; then
     echo "ERROR - Didn't find ${expected_run}"
@@ -57,3 +65,25 @@ clean
 
 
 
+make_dirs
+prjs_in_old_run_setup
+DEMUXED_DIR=${RUN_DIR} FASTQ_DIR=${FASTQ_DIR} ARCHIVED_DIR=${ARCHIVED_DIR} \
+  PROCESSED_SAMPLE_SHEET_DIR=${PROCESSED_SAMPLE_SHEET_DIR} RUNNAME=${RUN} ../../../templates/retrieve_all_sample_runs.sh
+OUTPUT_SS=run_samplesheet.txt
+EXPECTED_ENTRIES=1
+if [[ ${EXPECTED_ENTRIES} -eq $(cat ${OUTPUT_SS} | wc -l) ]]; then
+  echo "Expected number of Entries"
+else
+  echo "Expected ${EXPECTED_ENTRIES} entries. Found: $(cat ${OUTPUT_SS})"
+  exit 1
+fi
+EXPECTED_RUNS="ROSALIND_0002_FLOWCELL"
+for expected_run in ${EXPECTED_RUNS}; do
+  if [[ -z $(cat ${OUTPUT_SS} | grep ${expected_run} ) ]]; then
+    echo "ERROR - Didn't find ${expected_run}"
+    exit 1
+  else
+    echo "Found ${expected_run}"
+  fi
+done
+clean
