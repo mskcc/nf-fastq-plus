13fff33bc98bd0397540b57cb60e5675792286a8
David Streid <david.streid.nyc@gmail.com>
2021-07-06 17:36:16 -0400

    .*07822_Hg19.*: 10/30/17

--- bin/run_param_config.py
+++ bin/run_param_config.py
@@ -588,13 +588,6 @@ recipe_options_mapping_UNORDERED = {
         MSKQ: "no",
         MD: "yes"
     },
-    ".*07822_Hg19.*": {
-        # TODO - Delete "Twist_Exome" or change interval lists to be GRCh37
-        BAITS: "/home/igo/resources/BED-Targets/07822_Hg19_BAITS.iList",
-        TARGETS: "/home/igo/resources/BED-Targets/07822_Hg19_TARGETS.iList",
-        MSKQ: "yes",
-        MD: "yes"
-    },
     ".*08035_Hg19.*": {
         # TODO - Delete "Twist_Exome" or change interval lists to be GRCh37
         BAITS: "/home/igo/resources/BED-Targets/08035_hg19/08035_B_BAITS.interval_list",
