5e4cdf43c7622a512d417795e915f6c4aa02d781
DavidStreid <david.streid.nyc@gmail.com>
2021-09-27 10:18:58 -0400

    Piping to "sort | uniq" to eliminate duplicate params (#197)

--- templates/align_to_reference.sh
+++ templates/align_to_reference.sh
@@ -93,7 +93,7 @@ parse_param() {
   FILE=$1
   PARAM_NAME=$2
 
-  cat ${FILE}  | tr ' ' '\n' | grep -e "^${PARAM_NAME}=" | cut -d '=' -f2
+  cat ${FILE}  | tr ' ' '\n' | grep -e "^${PARAM_NAME}=" | cut -d '=' -f2 | sort | uniq
 }
 
 for LANE_PARAM_FILE in $(ls *${RUN_PARAMS_FILE}); do
--- templates/collect_alignment-summary_metrics.sh
+++ templates/collect_alignment-summary_metrics.sh
@@ -37,7 +37,7 @@ parse_param() {
   FILE=$1
   PARAM_NAME=$2
 
-  cat ${FILE}  | tr ' ' '\n' | grep -e "^${PARAM_NAME}=" | cut -d '=' -f2
+  cat ${FILE}  | tr ' ' '\n' | grep -e "^${PARAM_NAME}=" | cut -d '=' -f2 | sort | uniq
 }
 
 RUN_TAG=$(parse_param ${RUN_PARAMS_FILE} RUN_TAG)
