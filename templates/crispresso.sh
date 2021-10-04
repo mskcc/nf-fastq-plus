ed17c8885faff36135e2f945807e5303074e4874
David Streid <david.streid.nyc@gmail.com>
2021-10-04 12:35:49 -0400

    Passing formatted project dirname to crispresso script

--- templates/crispresso.sh
+++ templates/crispresso.sh
@@ -44,7 +44,6 @@ PROJECT_TAG=$(parse_param ${RUN_PARAMS_FILE} PROJECT_TAG)
 
 CRISPRESSO_OUTPUT_DIR=${STATSDONEDIR}/CRISPRESSO/${PROJECT_TAG}   # Where CRISPRESSO results will be written
 
-
 #################################################################
 #####     Step 2) Perform validation/error-checking         #####
 #################################################################
@@ -63,11 +62,11 @@ NUM_DIRS=$(echo ${PROJECT_DIR} | tr ' ' '\n' | wc -l)
 # Check dirs
 if [[ ${NUM_DIRS} -ne 1 ]]; then
   if [[ -f ${CRISPRESSO_OUTPUT_DIR}/checked.txt ]]; then
-    echo "Already sent email for missing ${PROJECT_TAG} - not sending"
+    echo "Already sent email for missing ${CRISPRESSO_OUTPUT_DIR} - not sending"
     exit 0
   else
-    subj="[ACTION REQUIRED] Resolve Crispresso for Project ${PROJECT_TAG}"
-    body="Identified more than one directory for ${PROJECT_TAG} in ${CRISPRESSO_EXCEL_INPUT_DIR}"
+    subj="[ACTION REQUIRED] Resolve Crispresso for Project ${CRISPRESSO_OUTPUT_DIR}"
+    body="Identified more than one directory for ${CRISPRESSO_OUTPUT_DIR} in ${CRISPRESSO_EXCEL_INPUT_DIR}"
     echo ${body} | mail -s "${subj}" ${DATA_TEAM_EMAIL}
     touch ${CRISPRESSO_OUTPUT_DIR}/checked.txt
   fi
@@ -78,11 +77,11 @@ NUM_EXCELS=$(echo ${EXCEL_FILE} | tr ' ' '\n' | wc -l)
 EXCEL_CHECKED_FILE=${CRISPRESSO_OUTPUT_DIR}/checked_excel.txt
 if [[ ${NUM_EXCELS} -ne 1 ]]; then
   if [[ -f ${EXCEL_CHECKED_FILE} ]]; then
-    echo "Already sent email for ambiguous excel ${PROJECT_TAG} - not sending"
+    echo "Already sent email for ambiguous excel ${CRISPRESSO_OUTPUT_DIR} - not sending"
     exit 0
   else
-    subj="[ACTION REQUIRED] Ambiguous Crispresso Excel for Project ${PROJECT_TAG} (Number: ${NUM_EXCELS})"
-    body="Identified more than one crispresso excel for ${PROJECT_TAG} in ${CRISPRESSO_EXCEL_INPUT_DIR}: ${EXCEL_FILE}"
+    subj="[ACTION REQUIRED] Ambiguous Crispresso Excel for Project ${CRISPRESSO_OUTPUT_DIR} (Number: ${NUM_EXCELS})"
+    body="Identified more than one crispresso excel for ${CRISPRESSO_OUTPUT_DIR} in ${CRISPRESSO_EXCEL_INPUT_DIR}: ${EXCEL_FILE}"
     echo ${body} | mail -s "${subj}" ${DATA_TEAM_EMAIL}
     touch ${EXCEL_CHECKED_FILE}
   fi
@@ -91,9 +90,9 @@ fi
 #################################################################
 ##### Step 3) Run Command - either Picard or python script  #####
 #################################################################
-echo "Running Crispresso on ${PROJECT_TAG}. Writing to ${CRISPRESSO_OUTPUT_DIR}"
+echo "Running Crispresso on ${CRISPRESSO_OUTPUT_DIR}. Writing to ${CRISPRESSO_OUTPUT_DIR}"
 RunCrisprAnalysis_REMIX.py -run ${RUN_TAG} \
-  -proj ${PROJECT_TAG} \
+  -proj ${CRISPRESSO_OUTPUT_DIR} \
   -edir ${CRISPRESSO_EXCEL_INPUT_DIR} \
   -fdir ${FASTQ_DIR} \
   -outdir ${CRISPRESSO_OUTPUT_DIR}
