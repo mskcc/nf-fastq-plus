a7bdac474a07d600d53349c7f30b3f4cbb56dcd0
DavidStreid <david.streid.nyc@gmail.com>
2021-10-04 08:24:59 -0400

    Adding "| sort | uniq"

--- templates/align_dragen.sh
+++ templates/align_dragen.sh
@@ -31,7 +31,7 @@ parse_param() {
   FILE=$1
   PARAM_NAME=$2
 
-  cat ${FILE}  | tr ' ' '\n' | grep -e "^${PARAM_NAME}=" | cut -d '=' -f2
+  cat ${FILE}  | tr ' ' '\n' | grep -e "^${PARAM_NAME}=" | cut -d '=' -f2 | sort | uniq
 }
 
 SAMPLE_PARAMS_FILE=$(ls *${RUN_PARAMS_FILE})
