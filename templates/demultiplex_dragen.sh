10300fa6218a8a9a7b4a0e90bc36d309119e0c54
David Streid <david.streid.nyc@gmail.com>
2021-10-12 16:48:55 -0400

    Copying PPG for DRAGEN demux & stats

--- templates/demultiplex_dragen.sh
+++ templates/demultiplex_dragen.sh
@@ -30,15 +30,7 @@ basename ${SAMPLESHEET}
 RUN_BASENAME=$(basename ${SAMPLESHEET} | grep -oP "(?<=[0-9]_)[A-Za-z_0-9-]+") # Capture after "[ANY NUM]_" (- ".csv")
 DEMUXED_DIR="${FASTQ_DIR}/${RUN_BASENAME}"
 
-PPG_CHECK=$(echo ${RUN_BASENAME} | rev | cut -d'_' -f1 | rev)
-echo "Checking if ${RUN_BASENAME} is a PED-PEG run. PPG_CHECK=${PPG_CHECK}"
-if [[ ${PPG_CHECK} == "PPG" ]]; then    # Check for whatever is defined as EXT_PPG in create_multiple_sample_sheets.py
-  # This is to get stats faster - we do not want to deliver this to the user
-  DEMUXED_DIR+="___DGN_RUN___DO_NOT_DELIVER"
-fi
-
 echo "Processing DRAGEN demultiplex Job SampleSheet=${SAMPLESHEET} RUN_BASENAME=${RUN_BASENAME} DEMUX_ALL=${DEMUX_ALL} DEMUXED_DIR=${DEMUXED_DIR}"
-
 if [[ "${DEMUX_ALL}" == "true" && -d ${DEMUXED_DIR}  ]]; then
   LOG="Skipping demux (DEMUX_ALL=${DEMUX_ALL}) of already demuxed directory: ${DEMUXED_DIR}"
   echo "${LOG}"
